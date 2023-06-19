294d0d2fe74ec076320501ab993f2a2916f88df6
tuanhq <haquoctuan0711@gmail.com>
2023-06-20 01:20:55 +0700

    config editor toolbar, create article

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import ReactDOM from 'react-dom';
 import './index.css';
+
+import 'react-quill/dist/quill.snow.css';
+
 import { App } from './components/App/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
